ad719273c66b79901d0e408157a624e6df55c51e
Geoffrey Irving <irving@naml.us>
2025-11-05 22:45:01 +0000

    More facts about Complex.sqrt

--- Series/Misc/Sqrt.lean
+++ Series/Misc/Sqrt.lean
@@ -1,4 +1,5 @@
 import Mathlib.Analysis.SpecialFunctions.Complex.LogDeriv
+import Mathlib.Analysis.SpecialFunctions.Pow.Complex
 
 /-!
 # The principle branch of the complex square root
@@ -14,6 +15,9 @@ noncomputable def Complex.sqrt (z : ℂ) : ℂ :=
   if z = 0 then 0 else exp (log z / 2)
 
 lemma Complex.sqrt_def (z : ℂ) : Complex.sqrt z = if z = 0 then 0 else exp (log z / 2) := rfl
+
+lemma Complex.sqrt_eq_cpow (z : ℂ) : z.sqrt = z ^ (2⁻¹ : ℂ) := by
+  simp only [sqrt, div_eq_mul_inv, cpow_def, inv_eq_zero, OfNat.ofNat_ne_zero, ↓reduceIte]
 
 /-- Away from zero, we can remove the if -/
 lemma Complex.sqrt_eventuallyEq (z0 : z ≠ 0) : sqrt =ᶠ[𝓝 z] fun z ↦ exp (log z / 2) := by
@@ -35,3 +39,7 @@ lemma ContDiffAt.csqrt (m : z ∈ slitPlane) {n : WithTop ℕ∞} : ContDiffAt �
   · simp only [sqrt_def, z0, ↓reduceIte, ← exp_nat_mul, Nat.cast_ofNat]
     ring_nf
     simp only [exp_log z0]
+
+/-- In the right halfplane, `sqrt (z ^ 2) = z` -/
+lemma Complex.sqrt_sq (r : 0 < z.re) : (z ^ 2).sqrt = z := by
+  simp only [Complex.sqrt_eq_cpow, Complex.sq_cpow_two_inv r]
